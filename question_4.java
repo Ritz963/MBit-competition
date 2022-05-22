00209b50038126fc20a110d833a48d95ca1f76ee
Ritvik Thakur <ritvik.thakur04@gmail.com>
2022-05-22 13:38:09 -0400

    master

--- question_4.java
+++ question_4.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class question_4 {
@@ -6,22 +7,37 @@ public class question_4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //program that takes an in integer and prints a string that is as long as the integer that is filled with random letters
+        //program that takes an in integer and prints a string that is as long as the integer that is filled with random lowercase letters
 
         int amount = input.nextInt();
         int length;
+        String answer = "";
+        String random = "";
+        char letter;
+        int randomNum;
+        ArrayList<String> words = new ArrayList<String>();
 
+        //keeps going for array length
         for(int x = 0; x<amount; x++){
             length = input.nextInt();
-            String random = "";
-            for(int y = 0; y<length; y++){
-                int randomNum = (int)(Math.random()*26);
-                char letter = (char)(randomNum + 97);
-                random += letter;
-            }
-            System.out.print(random);
+            
+            //keeps making random word
+            do{
+                random = "";
+                for(int y = 0; y<length; y++){
+                    randomNum = (int)(Math.random()*26);
+                    letter = (char)(randomNum + 97);
+                    random += letter;
+                }
+            }while(words.contains(random));
+            words.add(random);
         }
-    
+
+        for(int x = 0; x<words.size()-1; x++){
+            System.out.println(words.get(x));
+        }
+
+        System.out.println(words.get(words.size()-1));
 
     }
     
