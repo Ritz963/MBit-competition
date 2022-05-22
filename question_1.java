e872944df3a19f83c7a4df8f3b4beeeb37d832b3
Ritvik Thakur <ritvik.thakur04@gmail.com>
2022-05-22 12:36:36 -0400

    master

--- question_1.java
+++ question_1.java
@@ -1,3 +1,9 @@
 public class question_1 {
     
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Jono is a chimp");
+    }
+
 }
